370b0c18fe5a335f0f91fe7c926591bd93c1ffec
ssube <seansube@gmail.com>
2020-08-09 17:51:02 -0500

    fix: export create helper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ArgSourceOptions, loadArgs, readArgs } from './args';
-export { Config, ConfigOptions } from './config';
+export { Config, ConfigOptions, createConfig } from './config';
 export { EnvSourceOptions, loadEnv, readEnv } from './env';
 export { FileSourceOptions, loadFile, readFile } from './file';
 export { Schema, SchemaDef, SchemaResult } from './schema';
